efe8e822cca5992f488954eda3a14929225a3867
amu0922-cmd <amu0922@gmail.com>
2026-06-04 21:13:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-const CACHE_NAME = 'keiba-predict-v5';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Service Worker無効化
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  // 全てキャッシュせず直接フェッチ
-  e.respondWith(
-    fetch(e.request).catch(() => new Response('', { status: 408 }))
-  );
-});
+// fetchイベントを登録しない = キャッシュ一切なし
