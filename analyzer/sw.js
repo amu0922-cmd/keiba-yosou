8ed2092166a6f06850e4ab34a17c876d022e54a7
amu0922-cmd <amu0922@gmail.com>
2026-06-08 22:35:31 +0900

    Delete analyzer directory

--- analyzer/sw.js
+++
@@ -1,9 +0,0 @@
-// キャッシュなし・常に最新を取得
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-// fetchはそのままネットワークに流す（キャッシュしない）
